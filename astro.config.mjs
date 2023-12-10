a3567e37f88b793c343d40ba57cdc0cd502bee82
Jay Clouds <theo@wezel.ltd>
2023-12-10 23:50:27 +0100

    speed insight vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
+
+injectSpeedInsights();
 
 export default defineConfig({
   integrations: [react(), tailwind()]
