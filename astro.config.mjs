1ecdf8396375d2738ddac300ae52d5482ca2f13e
Jay Clouds <theo@wezel.ltd>
2023-12-11 00:20:03 +0100

    dependencies update vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@ import { defineConfig } from 'astro/config';
 import { injectSpeedInsights } from '@vercel/speed-insights';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
-injectSpeedInsights();
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  injectSpeedInsights();
+  output: "server",
+  adapter: vercel()
 });
