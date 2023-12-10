0dc24f3cd6402b4b115d0bd0ed1a64c5af4374c1
Jay Clouds <theo@wezel.ltd>
2023-12-11 00:28:36 +0100

    config change vercel speedinsight

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
-import { injectSpeedInsights } from '@vercel/speed-insights';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  injectSpeedInsights();
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    speedInsights: {
+      enabled: true,
+    },
+  }),
 });
