259f5e2f5bdffeb2fe6247692dce783dcc8b083d
Jay Clouds <theo@wezel.ltd>
2024-01-24 20:15:53 +0100

    vercel reinstall

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_JmbrBXEO.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/generic_XkOahC0t.mjs');
+
+export { page };
