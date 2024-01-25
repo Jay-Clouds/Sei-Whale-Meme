04782f44b11854d41c01d826b83124c70bb9165b
Jay Clouds <theo@wezel.ltd>
2024-01-25 11:18:02 +0100

    vercel config delete again

--- .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_T0NEKtIo.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/generic_VJCQPc3A.mjs');
-
-export { page };
