222b9aa2f05669dd7abe4e095b1eba725a59ab03
Jay Clouds <theo@wezel.ltd>
2024-01-25 22:09:35 +0100

    og

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_T0NEKtIo.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/generic_VJCQPc3A.mjs');
+
+export { page };
